8faf596e14245dd91c5a0b674f0883fba9110b95
GhaidaAl36 <ghaida3638@gmail.com>
2025-09-30 16:26:51 +0000

    fix betty-style + Correct output - case: 17

--- printf.c
+++ printf.c
@@ -12,57 +12,51 @@ int _printf(const char *format, ...)
 	va_list args;
 	int count = 0;
 	const char *p;
-    	char c;
-    	char *str;
+	char c;
+	char *str;
 
-    	va_start(args, format);
+	va_start(args, format);
 
-    	if (!format)
-        	return (-1);
+	if (!format)
+		return (-1);
 
-    	for (p = format; *p; p++)
-    	{
-        	if (*p == '%')
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
 		{
-            	p++;
-            	if (*p == 'c')
-            	{
-                	c = (char)va_arg(args, int);
-                	write(1, &c, 1);
-                	count++;
-            	}
-            	else if (*p == 's')
-            	{
-                	str = va_arg(args, char *);
-                	if (!str)
-                    	str = "(null)";
-                	while (*str)
-                	{
-                    	write(1, str, 1);
-                    	str++;
-                    	count++;
-                	}
-            	}
-            	else if (*p == '%')
-            	{
-                	c = '%';
-                	write(1, &c, 1);
-                	count++;
-            	}
-            	else
-            	{
-                	write(1, "%", 1);
-                	write(1, p, 1);
-                	count += 2;
-            	}
-        	}
-        	else
-        	{
-            	write(1, p, 1);
-            	count++;
-        	}
-    	}
+			p++;
+			if (*p == 'c')
+			{
+				c = (char)va_arg(args, int);
+				write(1, &c, 1);
+				count++;
+			}
+			else if (*p == 's')
+			{
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(null)";
+				while (*str)
+				{
+					write(1, str, 1);
+					str++;
+					count++;
+				}
+			}
+			else if (*p == '%')
+			{
+				c = '%';
+				write(1, &c, 1);
+				count++;
+			}
+		}
+		else
+		{
+			write(1, p, 1);
+			count++;
+		}
+	}
 
-    	va_end(args);
-    	return (count);
+	va_end(args);
+	return (count);
 }
