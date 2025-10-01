532f34f0a6ad78997abf0de50383f9d40deb124d
ako11927 <ako11927@helobertonstudents.com>
2025-10-01 13:35:12 +0000

    Add _printf.c and remove stray binary

---
+++ _printf.c
@@ -0,0 +1,72 @@
+#include "main.h"
+
+/**
+ * _printf - minimal printf clone handling %c, %s, %%, %d, %i
+ * @format: format string
+ *
+ * Return: number of characters printed, or -1 on error
+ */
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int i, count = 0;
+
+	if (format == 0)
+		return (-1);
+
+	va_start(ap, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			if (_putchar(format[i]) < 0) { count = -1; break; }
+			count++;
+			continue;
+		}
+
+		i++;
+		if (format[i] == '\0') { count = -1; break; }
+
+		if (format[i] == 'c')
+		{
+			char c = (char)va_arg(ap, int);
+			if (_putchar(c) < 0) { count = -1; break; }
+			count++;
+		}
+		else if (format[i] == 's')
+		{
+			const char *s = va_arg(ap, const char *);
+			int j;
+
+			if (s == 0) s = "(null)";
+			for (j = 0; s[j] != '\0'; j++)
+			{
+				if (_putchar(s[j]) < 0) { count = -1; break; }
+				count++;
+			}
+			if (count < 0) break;
+		}
+		else if (format[i] == '%')
+		{
+			if (_putchar('%') < 0) { count = -1; break; }
+			count++;
+		}
+		else if (format[i] == 'd' || format[i] == 'i')
+		{
+			int val = va_arg(ap, int);
+			int k = print_int((long)val);
+			if (k < 0) { count = -1; break; }
+			count += k;
+		}
+		else
+		{
+			/* Unknown specifier: print '%' then the char */
+			if (_putchar('%') < 0 || _putchar(format[i]) < 0) { count = -1; break; }
+			count += 2;
+		}
+	}
+
+	va_end(ap);
+	return (count);
+}
