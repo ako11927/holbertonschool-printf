029aed12f17cc376310c50e667260694ea9c61cc
ako11927 <ako11927@helobertonstudents.com>
2025-10-01 14:05:52 +0000

    Fix Betty style and documentation

--- _putchar.c
+++ _putchar.c
@@ -1,7 +1,16 @@
 #include "main.h"
 
+/**
+ * _putchar - writes a character to stdout
+ * @c: character to print
+ *
+ * Return: 1 on success, -1 on error (errno is set appropriately)
+ */
 int _putchar(char c)
 {
-	int r = write(1, &c, 1);
-	return (r == 1) ? 1 : -1;
+    int r = write(1, &c, 1);
+
+    if (r == 1)
+        return (1);
+    return (-1);
 }
--- print_int.c
+++ print_int.c
@@ -1,32 +1,40 @@
 #include "main.h"
 
-/* prints a signed long as decimal; returns number of chars printed, or -1 on error */
+/**
+ * print_int - prints a signed integer
+ * @n: number to print
+ *
+ * Description: Prints a signed decimal number using recursion.
+ * Handles negative values, including INT_MIN.
+ *
+ * Return: number of characters printed, or -1 on error
+ */
 int print_int(long n)
 {
-	int count = 0;
-	unsigned long u;
+    int count = 0;
+    unsigned long u;
 
-	if (n < 0)
-	{
-		if (_putchar('-') < 0)
-			return (-1);
-		count++;
-		u = (unsigned long)(-n); /* works even for INT_MIN by promoting before negation */
-	}
-	else
-	{
-		u = (unsigned long)n;
-	}
+    if (n < 0)
+    {
+        if (_putchar('-') < 0)
+            return (-1);
+        count++;
+        u = (unsigned long)(-n);
+    }
+    else
+    {
+        u = (unsigned long)n;
+    }
 
-	/* print recursively into a buffer-less stream using _putchar */
-	if (u / 10)
-	{
-		int k = print_int((long)(u / 10));
-		if (k < 0)
-			return (-1);
-		count += k;
-	}
-	if (_putchar((char)('0' + (u % 10))) < 0)
-		return (-1);
-	return (count + 1);
+    if (u / 10)
+    {
+        int k = print_int((long)(u / 10));
+
+        if (k < 0)
+            return (-1);
+        count += k;
+    }
+    if (_putchar((char)('0' + (u % 10))) < 0)
+        return (-1);
+    return (count + 1);
 }
